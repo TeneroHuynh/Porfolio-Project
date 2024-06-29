d1dc5bcb1dc3b748c39bf4edd0a917d500cf9e0c
Huỳnh Trang Vạn Phúc <Phuc2003cm2003@gmail.com>
2024-06-29 10:31:10 +0700

    Add files via upload

---
+++ [Tenero Huynh] SQL project code sample.sql
@@ -0,0 +1,12 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE 'employee_survey_data' ('EmployeeID' INTEGER,'EnvironmentSatisfaction' TEXT,'JobSatisfaction' TEXT,'WorkLifeBalance' TEXT);
+CREATE TABLE 'general_data' ('Age' INTEGER,'Attrition' TEXT,'BusinessTravel' TEXT,'Department' TEXT,'DistanceFromHome' INTEGER,'Education' INTEGER,'EducationField' TEXT,'EmployeeCount' INTEGER,'EmployeeID' INTEGER,'Gender' TEXT,'JobLevel' INTEGER,'JobRole' TEXT,'MaritalStatus' TEXT,'MonthlyIncome' INTEGER,'NumCompaniesWorked' TEXT,'Over18' TEXT,'PercentSalaryHike' INTEGER,'StandardHours' INTEGER,'StockOptionLevel' INTEGER,'TotalWorkingYears' TEXT,'TrainingTimesLastYear' INTEGER,'YearsAtCompany' INTEGER,'YearsSinceLastPromotion' INTEGER,'YearsWithCurrManager' INTEGER);
+CREATE TABLE 'manager_survey_data' ('EmployeeID' INTEGER,'JobInvolvement' INTEGER,'PerformanceRating' INTEGER);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
